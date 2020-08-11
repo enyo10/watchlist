33f280e88c1ed5bc6e77d80b94c428ce15ee793f
enyo10 <enyo10@yahoo.fr>
2020-08-11 18:26:36 +0200

    initial commit

---
+++ src/main/java/ch/enyo/springapp/whatchlist/WhatchlistApplication.java
@@ -0,0 +1,13 @@
+package ch.enyo.springapp.whatchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatchlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/enyo/springapp/whatchlist/WhatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.enyo.springapp.whatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
